22a94942f227f84f58f0aae16381d7500e2b0a9a
ghernandez24 <halex24a@bu.edu>
2021-05-26 23:13:53 -0400

    python tests

---
+++ manage.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+
+with open('home.html', 'r') as html_file:
+    content = html_file.read()
+
+    soup = BeautifulSoup(content, 'lxml' )
+    tags = soup.find('td')
+    print(tags)
